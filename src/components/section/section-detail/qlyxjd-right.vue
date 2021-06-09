ebf4e3278ec718809b6b981862e7317ff4a1e920
chenjing <pigkiko@hotmail.com>
2021-06-09 16:03:40 +0800

    涉及审批等

--- src/components/section/section-detail/qlyxjd-right.vue
+++ src/components/section/section-detail/qlyxjd-right.vue
@@ -18,6 +18,50 @@
         :color="item.color"
       ></progress-bar>
     </div>
+    <div class="sheji-items w-full">
+      <div class="sheji-item">
+        <img src="~@/assets/images/shenpi.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及审批</p>
+          <p><span>8567</span>次</p>
+        </div>
+      </div>
+      <div class="sheji-item">
+        <img src="~@/assets/images/qunzhong.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及群众</p>
+          <p><span>8567</span>人</p>
+        </div>
+      </div>
+      <div class="sheji-item">
+        <img src="~@/assets/images/company.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及企业</p>
+          <p><span>345</span>家</p>
+        </div>
+      </div>
+      <div class="sheji-item">
+        <img src="~@/assets/images/xiangmu.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及项目</p>
+          <p><span>537</span>个</p>
+        </div>
+      </div>
+      <div class="sheji-item">
+        <img src="~@/assets/images/wuzi.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及物资</p>
+          <p><span>43566</span>个</p>
+        </div>
+      </div>
+      <div class="sheji-item">
+        <img src="~@/assets/images/zijin.png" alt="" />
+        <div class="right flex-col">
+          <p>涉及资金</p>
+          <p><span>2,245,245</span>元</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,5 +119,34 @@ export default defineComponent({
   height: 318px;
   border-bottom: 1px solid #225d73;
   padding: 24px;
+}
+.sheji-items {
+  height: 242px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 28px 20px;
+  .sheji-item {
+    display: flex;
+    flex-direction: row;
+    width: 50%;
+    height: 44px;
+    margin-bottom: 20px;
+    img {
+      width: 44px;
+      height: 44px;
+      margin-right: 13px;
+    }
+    p {
+      font-size: 14px;
+      color: #acb4ba;
+      span {
+        font-size: 22px;
+        color: #fff;
+        margin-right: 8px;
+      }
+    }
+  }
 }
 </style>
